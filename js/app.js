818fde3a15ddb598464bddcd1107b45892b88b1e
Roshan <56087656+Roshan-16@users.noreply.github.com>
2021-06-30 20:21:12 +0530

    v2.0

---
+++ js/app.js
@@ -0,0 +1,173 @@
+tsParticles.load("ts1particles", {
+        interactivity: {
+          detectsOn: "window",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble"
+            }
+          },
+          modes: {
+            bubble: {
+              distance: 200,
+              duration: 2,
+              opacity: 0,
+              size: 0
+            },
+            grab: {
+              distance: 400
+            },
+            repulse: {
+              distance: 400
+            }
+          }
+        },
+        particles: {
+          color: {
+            value: "#2c3e50"
+          },
+          links: {
+            color: {
+              value: "#2c3e50"
+            },
+            distance: 150,
+            opacity: 0.4
+          },
+          move: {
+            attract: {
+              rotate: {
+                x: 600,
+                y: 600
+              }
+            },
+            enable: true,
+            outModes: {
+              bottom: "out",
+              left: "out",
+              right: "out",
+              top: "out"
+            },
+            random: true,
+            speed: 1
+          },
+          number: {
+            density: {
+              enable: true
+            },
+            value: 180
+          },
+          opacity: {
+            random: {
+              enable: true
+            },
+            value: {
+              min: 0,
+              max: 0.8
+            },
+            animation: {
+              enable: true,
+              speed: 1
+            }
+          },
+          size: {
+            random: {
+              enable: true
+            },
+            value: {
+              min: 1,
+              max: 3
+            },
+            animation: {
+              speed: 4,
+              minimumValue: 0.3
+            }
+          }
+        }
+});
+
+tsParticles.load("ts2particles", {
+        interactivity: {
+          detectsOn: "window",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble"
+            }
+          },
+          modes: {
+            bubble: {
+              distance: 200,
+              duration: 2,
+              opacity: 0,
+              size: 0
+            },
+            grab: {
+              distance: 400
+            },
+            repulse: {
+              distance: 400
+            }
+          }
+        },
+        particles: {
+          color: {
+            value: "#2c3e50"
+          },
+          links: {
+            color: {
+              value: "#2c3e50"
+            },
+            distance: 150,
+            opacity: 0.4
+          },
+          move: {
+            attract: {
+              rotate: {
+                x: 600,
+                y: 600
+              }
+            },
+            enable: true,
+            outModes: {
+              bottom: "out",
+              left: "out",
+              right: "out",
+              top: "out"
+            },
+            random: true,
+            speed: 1
+          },
+          number: {
+            density: {
+              enable: true
+            },
+            value: 180
+          },
+          opacity: {
+            random: {
+              enable: true
+            },
+            value: {
+              min: 0,
+              max: 0.8
+            },
+            animation: {
+              enable: true,
+              speed: 1
+            }
+          },
+          size: {
+            random: {
+              enable: true
+            },
+            value: {
+              min: 1,
+              max: 3
+            },
+            animation: {
+              speed: 4,
+              minimumValue: 0.3
+            }
+          }
+        }
+});
